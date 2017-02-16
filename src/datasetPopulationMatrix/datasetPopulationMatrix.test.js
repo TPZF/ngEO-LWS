4cc02d5093db510285eba719b2386822e0649947
irchad <irchad.alihoussen@telespazio.com>
2017-02-16 16:46:40 +0100

    test response are now stubed by nock

--- src/datasetPopulationMatrix/datasetPopulationMatrix.test.js
+++ src/datasetPopulationMatrix/datasetPopulationMatrix.test.js
@@ -2,33 +2,40 @@
 * Unit test file for service web dataset poupulation matrix to populate available dataset
 * It allow to test the REST service and the dataset poupulation matrix to populate available dataset
 */
+var nock = require('nock');
+var expect = require('chai').expect;
+//used to read the JSON file
+var fs = require("fs");
 var supertest = require("supertest");
-var should = require("should");
-var logger = require('../utils/logger');
-var app = require('../app');
-var assert = require('assert');
-var utils = require('../utils/utils');
 // This agent refers to PORT where program is runninng.
 var server = supertest("http://localhost:3000");
 
+var logger = require('../utils/logger');
+var assert = require('assert');
+var utils = require('../utils/utils');
 
 // UNIT test begin
-
 describe("IF-ngEO-datasetPopulationMatrix --> Unit test",function(){
-  var confData = {};
   
   it("should return the dataset population json file for the WEBC and verify some parameter in the json file",function(done){
-    //calling ADD api
+    //stun the response by sending our test configuration file
+    var contents = fs.readFileSync('./test_data/datasets-test-file.json');
+    //parse it
+    var jsonContent = JSON.parse(contents);
+
+    //stub the response in order to send our test file
+    nock("http://localhost:3000")
+      .get('/ngeo/datasetPopulationMatrix')
+      .reply(200, jsonContent);
+
     server
-    .get('/ngeo/datasetPopulationMatrix')
-    .expect("Content-type",/json/)
-    .expect(200)
-    .end(function(err,res){
-      var confData = JSON.parse(utils.removeComments(res.text));
-      //var confData = JSON.parse(res.text);
-      //to be modified whenever we have another test file
-      assert.equal(confData.datasetpopulationmatrix.criteriaTitles.length,6);
-      done();
-    });
-  });
+      .get('/ngeo/datasetPopulationMatrix')
+      .expect(200)
+      .end(function (err, res) {
+        var confData = JSON.parse(res.text);
+        //to be modified whenever we have another test file
+        assert.equal(confData.datasetpopulationmatrix.criteriaTitles.length,6);
+        done();
+      });
+  })
 });
--- src/webClientConfigurationData/webClientConfigurationData.test.js
+++ src/webClientConfigurationData/webClientConfigurationData.test.js
@@ -2,33 +2,42 @@
 * Unit test file for service web client configuration Data
 * It allow to test the REST service and the data the configuration should contain
 */
+var nock = require('nock');
+var expect = require('chai').expect;
+//used to read the JSON file
+var fs = require("fs");
 var supertest = require("supertest");
-var should = require("should");
-var logger = require('../utils/logger');
-var app = require('../app');
-var assert = require('assert');
-var utils = require('../utils/utils');
 // This agent refers to PORT where program is runninng.
 var server = supertest("http://localhost:3000");
 
+var logger = require('../utils/logger');
+var assert = require('assert');
+var utils = require('../utils/utils');
+
 
 // UNIT test begin
-
 describe("IF-ngEO-WebClientConfigurationData --> Unit test",function(){
-  var confData = {};
   
   it("should return the configuration json file for the WEBC and verify some parameter in the json file",function(done){
-    //calling ADD api
+    //stun the response by sending our test configuration file
+    var contents = fs.readFileSync('./test_data/configuration-test-file.json');
+    //parse it
+    var jsonContent = JSON.parse(contents);
+
+    //stub the response in order to send our test file
+    nock("http://localhost:3000")
+      .get('/ngeo/webClientConfigurationData')
+      .reply(200, jsonContent);
+
     server
-    .get('/ngeo/webClientConfigurationData')
-    .expect("Content-type",/json/)
-    .expect(200)
-    .end(function(err,res){
-      var confData = JSON.parse(utils.removeComments(res.text));
-      //to be modified whenever we have another test file
-      assert.equal(confData.tableView.directDownloadColumn,7);
-      assert.equal(confData.tableView.columnsDef.length,15);
-      done();
-    });
-  });
+      .get('/ngeo/webClientConfigurationData')
+      .expect(200)
+      .end(function (err, res) {
+        var confData = JSON.parse(res.text);
+        //to be modified whenever we have another test file
+        assert.equal(confData.tableView.directDownloadColumn,7);
+        assert.equal(confData.tableView.columnsDef.length,15);
+        done();
+      });
+  })
 });
