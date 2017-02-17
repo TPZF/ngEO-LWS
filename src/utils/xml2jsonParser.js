c90beee947fa05e13e52b02e866f1413ab21a1e2
Maxime <maxime.shubin@telespazio.com>
2017-02-17 11:23:53 +0100

    Add xml2json parser class
    In case of change the xml2json 3rd party library will be very handly

---
+++ src/utils/xml2jsonParser.js
@@ -0,0 +1,35 @@
+let xml2js = require('xml2js-expat');
+let logger = require('../utils/logger');
+
+/**
+ * Xml parser class designed to create the abstraction between the 3rd party lib
+ * and the rest of application
+ * Allows us to change quickly the implementation of xml2json parser
+ */
+class Xml2JsonParser {
+    constructor() {}
+
+    /**
+     * Parse the given xml
+     * @param {string} xml
+     *      Xml to parse
+     * @param {function} onSuccess
+     *      Success callback
+     * @param {function} onError
+     *      Error callback
+     */
+    parse(xml, onSuccess, onError) {
+        let startTime = Date.now();
+        let parser = new xml2js.Parser(function (result, error) {
+            logger.info('Time elapsed by expat lib : ', Date.now() - startTime);
+            if (!error) {
+                onSuccess(result);
+            } else {
+                onError('Error while parsing the xml', xml)
+            }
+        });
+        parser.parseString(xml);
+    }
+}
+
+module.exports = new Xml2JsonParser();
