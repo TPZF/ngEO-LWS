7b4de1c7bb0c8603258419baebffca379452e077
Maxime <maxime.shubin@telespazio.com>
2017-02-22 18:30:23 +0100

    Add collection service test

---
+++ src/collectionService/collectionService.test.js
@@ -0,0 +1,29 @@
+let logger = require('../utils/logger');
+let nock = require('nock');
+let fs = require("fs");
+
+let collectionService = require('./collectionService');
+
+describe("Collection service methods", function () {
+
+	let testCollectionId = 'Landsat57Merged';
+	it("should return the information about the given dataset/collection in parsed json format", function () {
+		// OpenSearch description document mock
+		let mockOsdd = fs.readFileSync(`../test_data/${testCollectionId}-osdd.xml`);
+		nock('https://sxcat.eox.at')
+			.get(`/opensearch/collections/${testCollectionId}`)
+			.reply(200, mockOsdd);
+
+		collectionService.info(testCollectionId, {
+			onSuccess: (result) => {
+				result.should.have.property('@');
+				result.should.have.property('Url').with.lengthOf(4);
+				result.Url[0].should.have.property('prm:Parameter').with.lengthOf(12);
+			}
+		});
+	});
+
+	it("should return the search response on the given collection in parsed json format", function(){
+		// TODO
+	});
+});
