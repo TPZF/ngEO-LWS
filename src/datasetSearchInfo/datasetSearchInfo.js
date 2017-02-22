e0920303d085eee40889ad298db456b56d7c0edc
Maxime <maxime.shubin@telespazio.com>
2017-02-22 11:20:07 +0100

    Add first draft for advanced attributes conversion

--- src/datasetSearchInfo/datasetSearchInfo.js
+++ src/datasetSearchInfo/datasetSearchInfo.js
@@ -1,20 +1,61 @@
-let request = require('request');
 let express = require('express');
 let _ = require('lodash');
-let Xml2JsonParser = require('../utils/xml2jsonParser');
 let collectionService = require('../collectionService/collectionService');
 
 let router = express.Router({
 	mergeParams: true
 });
-// middleware that is specific to this router
 router.use(function timeLog(req, res, next) {
 	next();
 });
 
+/**
+ * Convert backend XML format to ngEO WEBC format
+ * 
+ * NB: Only range & list types are handled currently
+ * TODO: Make distinction between checkbox & select box
+ */
+let buildParameter = function(parameter) {
+	let res;
+	if (parameter['prm:Option']) {
+		// Selectbox
+		res = {
+			"id": parameter['@'].name,
+			"type": "List",
+			"possibleValues": []
+		}
+		parameter['prm:Option'].forEach( (option) => {
+			res.possibleValues.push(option['@'].value);
+		} );
+	} else if ( parameter['@'].minInclusive && parameter['@'].maxInclusive ) {
+		res = {
+			"id": parameter['@'].name,
+			"type": "Integer",
+			"rangeMinValue": parameter['@'].minInclusive,
+			"rangeMaxValue": parameter['@'].maxInclusive
+		}
+	}
+	return res;
+}
+
+// Avoid geo-spatial & catalog parameters since these are not takin part in advanced attributes
+let omittedParameters = ["count", "offset", "bbox", "grel", "start", "end", "trel", "availabilityTime"];
 
 /**
- * Build response respecting protocol used by current version of WEBC
+ * Build advanced attributes in ngEO WEBC format
+ */
+let buildAttributes = function(parameters) {
+	let result = [];
+	parameters.forEach( (parameter) => {
+		if (omittedParameters.indexOf(parameter['@'].name) == -1) {
+			result.push(buildParameter(parameter));
+		}
+	} );
+	return result;
+}
+
+/**
+ * Build datasetInfo response respecting protocol used by current version of WEBC
  */
 let buildResponse = function (datasetId, inputJson) {
 	let url = inputJson.Url[0]; // Take the first one by default(GET/bbox)
@@ -30,7 +71,7 @@ let buildResponse = function (datasetId, inputJson) {
 			"description": inputJson.Description,
 			"keywords": [], // TODO
 			"downloadOptions": [], // TODO
-			"attributes": [], // TODO
+			"attributes": buildAttributes(url['prm:Parameter']),
 			"startDate": startDateConf['@'].minInclusive,
 			"endDate": stopDateConf['@'].maxInclusive
 		}
