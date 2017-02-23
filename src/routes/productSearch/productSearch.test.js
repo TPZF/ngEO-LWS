165d38f272246a7f91cf9a03e3f45739dba189e0
irchad <irchad.alihoussen@telespazio.com>
2017-02-23 16:23:26 +0100

    search fixed according to code enhancement'

--- src/routes/productSearch/productSearch.test.js
+++ src/routes/productSearch/productSearch.test.js
@@ -4,20 +4,35 @@ let assert = require('assert');
 let nock = require('nock');
 // This agent refers to PORT where program is runninng.
 let server = supertest("http://localhost:3000");
+let Xml2JsonParser = require('utils/xml2jsonParser');
 
 let configurationConverter = require('../../utils/backendConfigurationConverter');
 let fs = require("fs");
+
+//stun the response by sending our test configuration file
+let contents = fs.readFileSync('../test_data/backend-rep-Landsat57Merged-all-test-file.xml');
+
+let onSuccess = function (result)  {
+	let jsonProcessed = configurationConverter.convertToNgeoWebCFormat(result);
+	jsonProcessed.should.have.property('features').with.lengthOf(10);
+	for (var i = 0; i < jsonProcessed.features.length; i++) {
+		var aFeature = jsonProcessed.features[i];
+		aFeature.should.have.property('type').exactly('Feature');
+		aFeature.should.have.keys('id', 'type', 'properties', 'geometry');
+		var earthObsProp = aFeature.properties.EarthObservation;
+		earthObsProp.should.have.keys('phenomenonTime', 'procedure');
+	}
+};
+
+let onError = function (errorMessage) {
+	//res.status(500).send(errorMessage);
+};
 
 // UNIT test begin
 
 describe("IF-ngEO-productSearch --> Unit test", function () {
 
 	it("should return a search file in atom opensearch xml response, convert it into geojson webc compliant format and ensure that the number of product found in the stub xml file which are 10 are well processed as 10 geojson features, test also some mandatory parameters that a feature shall have", function (done) {
-		//stun the response by sending our test configuration file
-		let contents = fs.readFileSync('../test_data/backend-rep-Landsat57Merged-all-test-file.xml');
-		//parse it
-		//let jsonContent = JSON.parse(contents);
-
 		//specify the url to be intercepted
 		nock("http://localhost:3000")
 			//define the method to be intercepted
@@ -29,16 +44,7 @@ describe("IF-ngEO-productSearch --> Unit test", function () {
 			.get('/ngeo/catalogue/myTestFeatureCollectionId/search')
 			.expect(200)
 			.end(function (err, res) {
-				let jsonProcessed = configurationConverter.convertToNgeoWebCFormat(res.text);
-				jsonProcessed.should.have.property('features').with.lengthOf(10);
-				for (var i = 0; i < jsonProcessed.features.length; i++) {
-					var aFeature = jsonProcessed.features[i];
-					aFeature.should.have.property('type').exactly('Feature');
-					aFeature.should.have.keys('id', 'type', 'properties', 'geometry');
-
-					var earthObsProp = aFeature.properties.EarthObservation;
-					earthObsProp.should.have.keys('phenomenonTime', 'procedure');
-				}
+				Xml2JsonParser.parse(res.text,onSuccess,onError);
 				done();
 			});
 	})
