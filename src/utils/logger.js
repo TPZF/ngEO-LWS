d1c5eef00b0d610f4976d0fd3670bc961c441f4e
Maxime <maxime.shubin@telespazio.com>
2017-02-27 17:08:52 +0100

    Logger in prod configuration is writing into log file for now

--- src/utils/logger.js
+++ src/utils/logger.js
@@ -7,31 +7,34 @@ let Configuration = require('config');
 winston.emitErrs = true;
 
 let logger = new winston.Logger({
-	transports: [
-		// // Uncomment if need to debug in file
-		// new winston.transports.File({
-		//	 level: 'info',
-		//	 filename: './logs/all-logs.log',
-		//	 handleExceptions: true,
-		//	 json: true,
-		//	 maxsize: 5242880, //5MB
-		//	 maxFiles: 5,
-		//	 colorize: false
-		// }),
-		new winston.transports.Console({
-			level: Configuration.logger.level,
-			handleExceptions: true,
-			json: false,
-			colorize: true
-		})
-	],
 	exitOnError: false
 });
 
-module.exports = logger;
 
-module.exports.stream = {
-	write: function (message, encoding) {
-		logger.info(message);
-	}
-};
+if (Configuration.logger.outputPath) {
+	logger.configure({
+		transports: [ new winston.transports.File({
+			level: Configuration.logger.level,
+			filename: Configuration.logger.outputPath,
+			handleExceptions: true,
+			json: false,
+			maxsize: 5242880, //5MB
+			maxFiles: 5,
+			colorize: false
+		})]
+	});
+} else {
+	logger.configure({
+		transports: [new winston.transports.Console({
+			level: Configuration.logger.level,
+			handleExceptions: true,
+			json: false,
+			colorize: true,
+			log: function() {
+
+			}
+		})]
+	});
+}
+
+module.exports = logger;
