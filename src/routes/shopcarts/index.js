20040ea2042218238873491aabf5752afebbc7b1
Manoel <manoel.olivier@gmail.com>
2017-03-07 14:53:04 +0100

    Rewrite shopcarts route with mongodb database

--- src/routes/shopcarts/index.js
+++ src/routes/shopcarts/index.js
@@ -1,4 +1,4 @@
-let express = require('express');
+	let express = require('express');
 let logger = require('utils/logger');
 //let _ = require('lodash');
 
@@ -24,19 +24,30 @@ router.get('/', (req, res) => {
 
 	logger.debug('ShopCart list is calling');
 
-	MongoClient.connect(url, (err,db) => {
-		if (err) {
-			res.send(err);
-		}
-		let myCursor = db.collection('ShopCart').find().toArray((errDB, items) => {
-			if (errDB) {
-				res.send(errDB);
-			}
-			res.json({"shopCartList" : items});
-			db.close();
-		});
+	let myDB = null;
 
-	});
+	try {
+		// connect to mongodb
+		MongoClient.connect(url, (err,db) => {
+			if (err) throw err;
+			logger.debug('db connection ok');
+			myDB = db;
+			// find and return an array
+			let myCursor = myDB.collection('ShopCart').find().toArray((errDB, items) => {
+				if (errDB) throw errDB;
+				logger.debug('find all is done');
+				items.forEach((myItem, myIndex) => {
+					myItem.id = myItem._id;
+				});
+				res.json({"shopCartList" : items});
+				myDB.close();
+			});
+		});
+	}
+	catch(exc) {
+		if (myDB!==null) myDB.close();
+		res.status(400).send(exc);
+	}
 
 });
 
@@ -47,13 +58,17 @@ router.post('/', (req,res) => {
 
 	logger.debug('ShopCart create is calling');
 
+	if (!checkRequest(req)) {
+		res.status(400).json("Request is not valid");
+		return;
+	}
+
 	let myDB = null;
 
-	if (!req.params.id && !req.body.id && req.body.createShopcart && req.body.createShopcart.shopcart && req.body.createShopcart.shopcart.name != "") {
-
-		MongoClient
-		.connect(url)
-		.then((db) => {
+	try {
+		// connect to mongodb
+		MongoClient.connect(url, (errConnect, db) => {
+			if (errConnect) throw errConnect;
 			logger.debug('db connection ok');
 			myDB = db;
 			let myQuery = {
@@ -64,49 +79,29 @@ router.post('/', (req,res) => {
 					$eq : req.body.createShopcart.shopcart.userId
 				}
 			};
-			return myDB.collection('ShopCart').findOne(myQuery);
-		}).then((respFindOne) => {
-			logger.debug('find ok');
-			if (respFindOne) {
-				return Promise.reject({status: 100, data: null, message: 'This shopcart already exists...'});
-			}
-			return myDB.collection('ShopCart').insertOne(req.body.createShopcart.shopcart);
-		})
-		.then((resp) => {
-			if (myDB) {
-				myDB.close();
-			}
-			let idCreated = resp.insertedId;
-			res.setHeader('Location', '/ngeo/shopcarts/' + idCreated)
-			res.status(201).json(idCreated);
-		})
-		.catch((err) => {
-			logger.debug('Oups! error !');
-			logger.debug(err);
-			if (myDB) {
-				myDB.close();
-			}
-			if (err.status) {
-				res.status(400).json(err.message);
-			} else {
-				res.send(400).json(
-					{
-						"status": 400,
-						"data": null,
-						"message": "Request is not valid"
-					}
-				);
-			}
+			// findOne shopcart with that name for this user
+			myDB.collection('ShopCart').findOne(myQuery, (errFind, resultFind) => {
+				if (errFind) throw errFind;
+				logger.debug('findOne is done.');
+				if (resultFind) {
+					res.status(400).json('This shopcart already exists.');
+				} else {
+					// insert shopcart
+					myDB.collection('ShopCart').insertOne(req.body.createShopcart.shopcart, (errInsert, resultInsert) => {
+						if (errInsert) throw errInsert;
+						logger.debug('insertOne is done.');
+						myDB.close();
+						let idCreated = resultInsert.insertedId;
+						res.setHeader('Location', '/ngeo/shopcarts/' + idCreated)
+						res.status(201).json(idCreated);
+					})
+				}
+			});
 		});
-
-	} else {
-		res.status(400).json(
-			{
-				"status": 400,
-				"data": null,
-				"message": "Request is not valid"
-			}
-		);
+	}
+	catch(exc) {
+		if (myDB!==null) myDB.close();
+		res.status(400).send(exc);
 	}
 	
 });
@@ -118,20 +113,23 @@ router.put('/:shopcart_id', (req,res) => {
 
 	logger.debug('ShopCart update is calling');
 
+	if (!checkRequest(req)) {
+		res.status(400).json("Request is not valid");
+		return;
+	}
+
 	let myDB = null;
+	let idToUpdate = req.params.shopcart_id;
 
-	if (req.params.shopcart_id && !req.body.id && req.body.createShopcart && req.body.createShopcart.shopcart && req.body.createShopcart.shopcart.name != "") {
-
-		MongoClient
-		// connection
-		.connect(url)
-		// find a shopcart with this name on another shopcart
-		.then((db) => {
+	try {
+		// connect to mongodb
+		MongoClient.connect(url, (errConnect, db) => {
+			if (errConnect) throw errConnect;
 			logger.debug('db connection ok');
 			myDB = db;
 			let myQuery = {
 				"_id": {
-					$ne: ObjectId(req.params.shopcart_id)
+					$ne: ObjectId(idToUpdate)
 				},
 				"name": {
 					$eq: req.body.createShopcart.shopcart.name
@@ -140,43 +138,40 @@ router.put('/:shopcart_id', (req,res) => {
 					$eq : req.body.createShopcart.shopcart.userId
 				}
 			};
-			return myDB.collection('ShopCart').findOne(myQuery);
-		})
-		// update if not found
-		.then((respFindOne) => {
-			if (respFindOne) {
-				return Promise.reject('ShopCart with this name already exists for another shopcart !');
-			}
-			let myFilter = {
-				"_id": ObjectId(req.params.shopcart_id)
-			};
-			let myUpdate = {
-				$set: {
-					"name": req.body.createShopcart.shopcart.name,
-					"isDefault": req.body.createShopcart.shopcart.isDefault
+			// findOne shopcart with this name and userId but another id !
+			myDB.collection('ShopCart').findOne(myQuery, (errFind, resultFind) => {
+				if (errFind) throw errFind;
+				if (resultFind) {
+					res.status(400).json('ShopCart with this name already exists for another shopcart !');
+				} else {
+					// update if not found
+					let myFilter = {
+						"_id": ObjectId(idToUpdate)
+					};
+					let myUpdate = {
+						$set: {
+							"name": req.body.createShopcart.shopcart.name,
+							"isDefault": req.body.createShopcart.shopcart.isDefault
+						}
+					};
+					myDB.collection('ShopCart').updateOne(myFilter, myUpdate, (errUpdate, resultUpdate) => {
+						if (errUpdate) throw errUpdate;
+						myDB.close();
+						if (resultUpdate.modifiedCount === 0) {
+							res.status(404).json('Not found !');
+						} else if (resultUpdate.modifiedCount === 1) {
+							res.json('updated');
+						} else {
+							throw 'ShopCart id is not unique !';
+						}
+					});
 				}
-			};
-			return myDB.collection('ShopCart').updateOne(myFilter, myUpdate);
+			});
 		})
-		// send response
-		.then((resp) => {
-			logger.debug('update ok');
-			if (myDB) {
-				myDB.close();
-			}
-			res.send({message: 'ShopCart updated !'});
-		})
-		// send error
-		.catch((err) => {
-			logger.debug('Oups! error !');
-			if (myDB) {
-				myDB.close();
-			}
-			res.send(err);
-		});
-
-	} else {
-		res.send({"message": "Datas not expected..."});
+	}
+	catch(exc) {
+		if (myDB!==null) myDB.close();
+		res.status(400).send(exc);
 	}
 	
 });
@@ -188,51 +183,76 @@ router.delete('/:shopcart_id', (req,res) => {
 
 	logger.debug('ShopCart delete is calling');
 
+	if (!checkRequest(req)) {
+		res.status(400).json("Request is not valid");
+		return;
+	}
+
 	let myDB = null;
 	let idToDelete = req.params.shopcart_id;
 
-	if (idToDelete) {
+	logger.debug('id = ' + idToDelete);
 
-		logger.debug('id = ' + idToDelete);
-
-		MongoClient
-		// connection
-		.connect(url)
-		// find a shopcart with this name on another shopcart
-		.then((db) => {
+	try {
+		// connect to mongodb
+		MongoClient.connect(url, (errConnect, db) => {
+			if (errConnect) throw errConnect;
 			logger.debug('db connection ok');
 			myDB = db;
 			let myFilter = {
 				"_id": ObjectId(idToDelete)
 			};
-			return myDB.collection('ShopCart').deleteOne(myFilter);
-		})
-		// send response
-		.then((resp) => {
-			if (myDB) {
+			// deleteOne shopcart with that id
+			myDB.collection('ShopCart').deleteOne(myFilter, (errDelete, resultDelete) => {
+				if (errDelete) throw errDelete;
 				myDB.close();
-			}
-			if (resp.deletedCount === 0) {
-				res.status(404).json('Not found !');
-			} else if (resp.deletedCount === 1) {
-				logger.debug('204 - delete ok for ' + idToDelete);
-				res.status(204);
-			}
+				if (resultDelete.deletedCount === 0) {
+					res.status(404).json('Not found !');
+				} else if (resultDelete.deletedCount === 1) {
+					logger.debug('204 - delete ok for ' + idToDelete);
+					res.status(204).send();
+				} else {
+					throw 'ShopCart id is not unique !';
+				}
+			});
+			
 		})
-		// send error
-		.catch((err) => {
-			logger.debug('Oups! error !');
-			if (myDB) {
-				myDB.close();
-			}
-			res.status(400).json({"message": err});
-		});
-
-	} else {
-		res.send({"message": "Datas not expected..."});
 	}
-	
+	catch (exc) {
+		if (myDB!==null) myDB.close();
+		res.status(400).send(exc);
+	}
+
 });
+
+function checkRequest(request) {
+	// only for put and post methods
+	if ((request.method === 'POST') || (request.method==='PUT')) {
+		if (!request.body.createShopcart) {
+			return false;
+		}
+		if (!request.body.createShopcart.shopcart) {
+			return false;
+		}
+		if (!request.body.createShopcart.shopcart.name) {
+			return false;
+		}
+		if (request.body.createShopcart.shopcart.name==='') {
+			return false;
+		}
+	}
+	// only for put and delete methods, check param id
+	if (((request.method === 'PUT') || (request.method === 'DELETE')) && !(request.params.shopcart_id)) {
+		return false;
+	}
+	// only for put and delete methods, check param id if 12 bytes
+	let patt = new RegExp(/^[a-fA-F0-9]{24}$/);
+	if (((request.method === 'PUT') || (request.method === 'DELETE')) && (!patt.test(request.params.shopcart_id))) {
+		return false;
+	}
+
+	return true;
+}
 
 
 module.exports = router;
