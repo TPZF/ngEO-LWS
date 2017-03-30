a1dfece80efddeb84fac1ef947504491ef295463
Manoel <manoel.olivier@gmail.com>
2017-03-30 11:09:06 +0200

    Unable browse button in map popup

--- public/js/map/map.js
+++ public/js/map/map.js
@@ -4179,7 +4179,7 @@ var MapPopup = function(container) {
 		});
 
 	// Browse
-	btn = $("<button data-icon='browse' data-iconpos='notext' data-role='button' data-inline='true' data-mini='true'>Display browse</button>")
+	/*btn = $("<button data-icon='browse' data-iconpos='notext' data-role='button' data-inline='true' data-mini='true'>Display browse</button>")
 		.appendTo(element.find("#mpButtons"))
 		.click(function() {
 			var isSelected = $(this).parent().hasClass('ui-btn-active');
@@ -4199,7 +4199,6 @@ var MapPopup = function(container) {
 				}
 			}
 		});
-
 	// Multiple browse management
 	btn = $("<button data-icon='browse-multiple' data-iconpos='notext' data-role='button' data-inline='true' data-mini='true'>Multiple browse management</button>")
 		.appendTo(element.find("#mpButtons"))
@@ -4209,6 +4208,7 @@ var MapPopup = function(container) {
 				featureCollection: products[0]._featureCollection
 			});
 		});
+		*/
 
 	// DAR
 	btn = $("<button data-icon='save' data-iconpos='notext' data-role='button' data-inline='true' data-mini='true'>Retrieve product</button>")
