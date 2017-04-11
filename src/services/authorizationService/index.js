29e877b2ccaab8e8df99ac7b37f5267130c43516
Manoel <manoel.olivier@gmail.com>
2017-04-11 10:23:20 +0200

    Add comments on service

--- src/services/authorizationService/index.js
+++ src/services/authorizationService/index.js
@@ -2,25 +2,38 @@ let Logger = require('utils/logger'),
 	Configuration = require('config');
 
 /**
- * Database service
- * Interface with mongodb service
+ * Authorization service
  */
 class AuthorizationService {
 
-	constructor() {
-	}
+	constructor() {	}
 
+    /**
+     * Check if the user has right on an object based on myCollection, myDocumentId and myUserId
+     * 
+     * @function isAuthorized
+     * @param {object} myResponse 
+     * @param {object} myDataBaseService 
+     * @param {string} myCollection 
+     * @param {string} myDocumentId 
+     * @param {string} myUserId 
+     * @param {function} myCallBackFn 
+     */
     isAuthorized(myResponse, myDataBaseService, myCollection, myDocumentId, myUserId, myCallBackFn) {
 
+        // callback function after search
         let cbAfterSearch = function(response) {
+            // bad response
             if (response.code !== 0) {
                 myResponse.status(response.code).json(response.datas);
                 return;
             }
+            // length of datas !== 1 --> error : too many or not enough
             if (response.datas.length !== 1) {
                 myResponse.status(404).json('Not found');
                 return;
             }
+            // if userId of object is different --> unauthorized
             if (response.datas[0].userId !== myUserId) {
                 myResponse.status(401).json('Unauthorized');
                 return;
@@ -28,10 +41,12 @@ class AuthorizationService {
             myCallBackFn();
         }
 
+        // json query filter based on _id
         let jsonQueryForfilter = {
             _id: myDocumentId
         };
 
+        // search and callback
         myDataBaseService.search(myCollection, jsonQueryForfilter, 0, 1, cbAfterSearch);
 
     }
