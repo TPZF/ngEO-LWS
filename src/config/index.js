ca1815127d362447f24f5b31cb08fae0c5ef3c99
Manoel <manoel.olivier@gmail.com>
2017-04-18 08:46:54 +0200

    Add a commons configuration file

--- src/config/index.js
+++ src/config/index.js
@@ -2,21 +2,25 @@ let process = require('process');
 
 class Configuration {
 	constructor() {
+		// just only in dev mode
+		process.env.NODE_TLS_REJECT_UNAUTHORIZED = "0";
 		console.log(`NODE_ENV: ${process.env.NODE_ENV}`);
 		switch (process.env.NODE_ENV) {
 			case 'development':
-				Object.assign(this, require('./development'), require('./account'));
+				Object.assign(this, require('./development'));
 				break;
 			case 'test':
-				Object.assign(this, require('./test'), require('./account'));
+				Object.assign(this, require('./test'));
 				break;
 			case 'production':
-				Object.assign(this, require('./production'), require('./account'));
+				Object.assign(this, require('./production'));
 				break;
 			default:
 				console.error(`Unrecognized NODE_ENV: ${process.env.NODE_ENV} using development configuration by default`);
-				Object.assign(this, require('./development.json'), require('./account'));
+				Object.assign(this, require('./development.json'));
 		}
+		// add account.json and commons.json configuration files
+		Object.assign(this, require('./account'), require('./commons'));
 	}
 }
 
