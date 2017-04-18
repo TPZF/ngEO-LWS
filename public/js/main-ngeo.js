4c87a39b47d0faccb937a49c922761e7c87c5d82
Manoel <manoel.olivier@gmail.com>
2017-04-18 16:50:57 +0200

    Add export widget for searchResults

--- public/js/main-ngeo.js
+++ public/js/main-ngeo.js
@@ -10062,10 +10062,58 @@ module.exports = DownloadOptionsWidget;
 });
 
 require.register("searchResults/widget/exportWidget", function(exports, require, module) {
+/**
+ * export widget module
+ * Used to display the supported export formats. 
+ */
+var Configuration = require('configuration');
+var ExportView = require('searchResults/view/exportView');
+var ngeoWidget = require('ui/widget');
 
+var ExportWidget = function(featureCollection) {
+
+	var parentElement = $('<div id="exportPopup">');
+	var element = $('<div id="exportPopupContent"></div>');
+	element.css('min-width', '200px');
+	element.appendTo(parentElement);
+	parentElement.appendTo('.ui-page-active');
+	parentElement.ngeowidget({
+		title: "Export",
+		// Reinit the standing order when the widget is closed (FL: is it really needed?)
+		hide: function() {
+			parentElement.remove();
+		}
+	});
+
+	var exportView = new ExportView({
+		model: featureCollection,
+		el: element
+	});
+
+	/**
+	 *	Open the popup
+	 */
+	this.open = function() {
+
+		exportView.render();
+
+		//trigger jqm styling
+		parentElement.ngeowidget("show");
+	};
+
+	/**
+	 *	For the moment not used since the popup can be 
+	 *	closed by clicking out side its content.
+	 */
+	this.close = function() {
+		parentElement.ngeowidget("hide");
+	};
+};
+
+module.exports = ExportWidget;
 });
 
-;require.register("searchResults/widget/multipleBrowseWidget", function(exports, require, module) {
+require.register("searchResults/widget/multipleBrowseWidget", function(exports, require, module) {
 /**  
  * Multiple browse selection widget module  
  */
