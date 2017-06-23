fa7ada904a4b703ed8526f4d05ec77b65f5fb2bc
Manoel <manoel.olivier@gmail.com>
2017-06-23 14:16:02 +0200

    Check authorization when creating simple data access request

--- src/routes/simpleDataAccessRequests/index.js
+++ src/routes/simpleDataAccessRequests/index.js
@@ -1,15 +1,19 @@
 // CORE
 let express = require('express');
+let ObjectId = require('mongodb').ObjectID;
 let _ = require('lodash');
 
 // UTILS
 let Logger = require('utils/logger');
 
 // SERVICES
+let AuthenticationService = require('services/authenticationService');
+let AuthorizationService = require('services/authorizationService');
 let DatabaseService = require('services/databaseService');
 
 // CONST
-let DATA_ACCESS_REQUEST_STATUS_NAME = 'DataAccessRequestStatus';
+const DATA_ACCESS_REQUEST_STATUS_NAME = 'DataAccessRequestStatus';
+const DOWNLOAD_MANAGER_NAME = 'DownloadManager';
 
 /**
  * check request inputs (method, params, values, ...)
@@ -34,6 +38,10 @@ function _checkRequest(request) {
 		}
 		if (!request.body.simpledataaccessrequest.requestStage) {
 			Logger.debug('no requestStage for simpledataaccessrequest');
+			return false;
+		}
+		if (!request.body.simpledataaccessrequest.downloadLocation) {
+			Logger.debug('no downloadLocation for simpledataaccessrequest');
 			return false;
 		}
 	}
@@ -64,61 +72,79 @@ router.put('/', (req, res) => {
 		return;
 	}
 
-	let response = {
-		dataAccessRequestStatus: {
-			ID: "NO_ID-validation_stage",
-			type: "Simple Data Access Request",
-			status: 4,
-			message: "Data Access Request validated",
-			dlManagerId: req.body.simpledataaccessrequest.downloadLocation.DownloadManagerId,
-			downloadDirectory: req.body.simpledataaccessrequest.downloadLocation.DownloadDirectory,
-			productStatuses: []
-		}
-	};
-
-	_.each(req.body.simpledataaccessrequest.productURLs, function (product) {
-		product.productStatus = 'NOT_STARTED';
-		product.percentageCompleted = '0';
-		response.dataAccessRequestStatus.productStatuses.push(product);
-	});
-
-	// validation
-	if (req.body.simpledataaccessrequest.requestStage === 'validation') {
-		response.dataAccessRequestStatus.status = 4;
-		res.status(200).json(response);
+	// check if download manager id exists
+	let downloadManagerId = req.body.simpledataaccessrequest.downloadLocation.DownloadManagerId;
+	if (typeof downloadManagerId === 'undefined') {
+		Logger.debug('no DownloadManagerId in downloadLocation attribute for this simple DAR');
+		res.status(400).json("Request is not valid");
 		return;
 	}
 
-	// confirmation
-	if (req.body.simpledataaccessrequest.requestStage === 'confirmation') {
-		response.dataAccessRequestStatus.name = req.body.simpledataaccessrequest.name;
-		response.dataAccessRequestStatus.status = 0;
-		// define call back function after creating
-		// send response
-		let cbAfterCreate = function (response) {
-			if (response.code !== 0) {
-				res.status(response.code).json(response.datas);
-			} else {
-				response.datas.ID = response.datas.id;
-				delete response.datas._id;
-				delete response.datas.id;
-				res.setHeader('Location', '/ngeo/dataAccessRequestStatuses/' + response.datas.ID)
-				res.status(201).json({ "dataAccessRequestStatus": response.datas });
+	let cbAfterCheckAuthorization = function () {
+		let response = {
+			dataAccessRequestStatus: {
+				ID: "NO_ID-validation_stage",
+				type: "Simple Data Access Request",
+				status: 4,
+				message: "Data Access Request validated",
+				dlManagerId: downloadManagerId,
+				downloadDirectory: req.body.simpledataaccessrequest.downloadLocation.DownloadDirectory,
+				productStatuses: []
 			}
 		};
+		_.each(req.body.simpledataaccessrequest.productURLs, function (product) {
+			product.productStatus = 'NOT_STARTED';
+			product.percentageCompleted = '0';
+			response.dataAccessRequestStatus.productStatuses.push(product);
+		});
 
-		// define insertedItem
-		let myInsertItem = response.dataAccessRequestStatus;
+		// validation
+		if (req.body.simpledataaccessrequest.requestStage === 'validation') {
+			response.dataAccessRequestStatus.status = 4;
+			res.status(200).json(response);
+			return;
+		}
 
-		// define query to find if item is already in database
-		let myQueryItemAlreadyExists = {
-			exist: false // Not already exists in all cases
-		};
+		// confirmation
+		if (req.body.simpledataaccessrequest.requestStage === 'confirmation') {
+			response.dataAccessRequestStatus.name = req.body.simpledataaccessrequest.name;
+			response.dataAccessRequestStatus.status = 0;
+			// define call back function after creating
+			// send response
+			let cbAfterCreate = function (response) {
+				if (response.code !== 0) {
+					res.status(response.code).json(response.datas);
+				} else {
+					response.datas.ID = response.datas.id;
+					delete response.datas._id;
+					delete response.datas.id;
+					res.setHeader('Location', '/ngeo/dataAccessRequestStatuses/' + response.datas.ID)
+					res.status(201).json({ "dataAccessRequestStatus": response.datas });
+				}
+			};
 
-		// call create service for database
-		DatabaseService.create(DATA_ACCESS_REQUEST_STATUS_NAME, myInsertItem, myQueryItemAlreadyExists, cbAfterCreate);
+			// define insertedItem
+			let myInsertItem = response.dataAccessRequestStatus;
+
+			// define query to find if item is already in database
+			let myQueryItemAlreadyExists = {
+				exist: false // Not already exists in all cases
+			};
+
+			// call create service for database
+			DatabaseService.create(DATA_ACCESS_REQUEST_STATUS_NAME, myInsertItem, myQueryItemAlreadyExists, cbAfterCreate);
+		}
 	}
 
+	// check authorization with user id
+	AuthorizationService.isAuthorized(
+		res,
+		DatabaseService,
+		DOWNLOAD_MANAGER_NAME,
+		ObjectId(downloadManagerId),
+		AuthenticationService.getUserId(req),
+		cbAfterCheckAuthorization
+	);
 
 });
 
