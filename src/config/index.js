0e1ff6ac09c6cddc053cbef3d57d9e160e2706b9
Alihoussen Irchad <ialihoussen@telespazio.corp>
2017-07-05 16:09:59 +0200

    process.env.NODE_TLS_REJECT_UNAUTHORIZED was added because some catalog url is rejected otherelse

--- src/config/index.js
+++ src/config/index.js
@@ -3,7 +3,8 @@ let process = require('process');
 class Configuration {
 	constructor() {
 		// FIXME : just only in dev mode
-		//process.env.NODE_TLS_REJECT_UNAUTHORIZED = "0";
+		//if this is not set then targeting some feed catalog doesnot work because rejected
+		process.env.NODE_TLS_REJECT_UNAUTHORIZED = "0";
 		console.log(`NODE_ENV: ${process.env.NODE_ENV}`);
 		switch (process.env.NODE_ENV) {
 			case 'development':
